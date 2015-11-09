31a21ee58cd97c8713be4b05e7d60e093b9a8842
ArturKarbone <artur.karbone@gmail.com>
2015-11-09 23:20:14 +0200

    Experimenting with no 'ifs' concept in .NET. Inspired by yet another stellar talk by Sandi Metz, called  "Nothing is Something" (RailsConf 2

---
+++ BoolExtensions.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SubstituteIFsWithMessages
+{
+    public static class BoolExtensions
+    {
+        public static void IfTrue(this bool value, Action action)
+        {
+            if (value)
+            {
+                action();
+            }
+        }
+
+        public static void IfFalse(this bool value, Action action)
+        {
+            if (!value)
+            {
+                action();
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SubstituteIFsWithMessages
+{
+    class Program
+    {        
+        static void Main(string[] args)
+        {
+            GatherConditions()
+                .ToList()
+                .ForEach(condition =>
+                {
+                    Test1(condition);
+                    Test2(condition);
+                });
+
+            Console.ReadLine();
+        }
+
+        static IEnumerable<bool> GatherConditions() {
+            yield return 1 == 1;
+            yield return 1 == 2;
+        }
+
+
+        private static void Test1(bool condition)
+        {
+            Console.WriteLine("Test1");
+            if (condition)
+            {
+                Console.WriteLine("That's true");
+            }
+            else
+            {
+                Console.WriteLine("That's false");
+            }
+        }
+
+        private static void Test2(bool condition)
+        {
+            Console.WriteLine("Test2");
+            condition.IfTrue(() => Console.WriteLine("That's true"));
+            condition.IfFalse(() => Console.WriteLine("That's false"));            
+        }
+    }
+}
